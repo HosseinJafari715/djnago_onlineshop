88f58ed18c52f0233ee83fc0c29f5235d859e8fb
Hossein <hosseinjafari6174@gmail.com>
2025-04-11 00:49:41 +0330

    add crispy forms with bootstrap

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third-party apps
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    # local apps
     'accounts',
     'pages',
 ]
@@ -136,3 +141,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms settings
+
+CRISPY_TEMPLATE_PACK='bootstrap'
+
+# For Bootstrap 5
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
