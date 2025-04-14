34a19ea079e186181e22831e9ad8d22be4906ef2
Hossein <hosseinjafari6174@gmail.com>
2025-04-14 22:23:10 +0330

    change some accounts templates

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
 
 # Application definition
@@ -136,6 +136,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "hosseinjafari6174@gmail.com"
+# EMAIL_HOST_PASSWORD = "otuz egkl znuo mofm"
+
 
 
 # Internationalization
