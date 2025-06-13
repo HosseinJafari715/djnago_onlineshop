abcc8a00a4f182c24e62bd902ac9c9dbc2290431
HosseinJafari715 <hosseinjafari6174@gmail.com>
2025-06-14 01:57:18 +0330

    some change in payment/urls.py

--- payment/urls.py
+++ payment/urls.py
@@ -6,12 +6,11 @@ app_name = 'payment'
 
 
 urlpatterns = [
-    # path('process/', payment_process, name='payment_process'),
-    # path('callback/', payment_callback, name='payment_callback'),
+    path('process/', payment_process, name='payment_process'),
+    path('callback/', payment_callback, name='payment_callback'),
 
     # Sandbox
-    path('process/', payment_process_sandbox, name='payment_process'),
-    path('callback/', payment_callback_sandbox, name='payment_callback'),
-
+    # path('process/', payment_process_sandbox, name='payment_process'),
+    # path('callback/', payment_callback_sandbox, name='payment_callback'),
 ]
 
